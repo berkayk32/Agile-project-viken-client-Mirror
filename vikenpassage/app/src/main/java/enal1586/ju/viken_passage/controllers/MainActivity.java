5277d2fe5cecab556a9b00d8a8c622d54e66e64c
yllisahiti@gmail.com <yllisahiti@gmail.com>
2019-04-09 19:36:20 +0200

    Updated firebase settings

--- vikenpassage/app/src/main/java/enal1586/ju/viken_passage/controllers/MainActivity.java
+++ vikenpassage/app/src/main/java/enal1586/ju/viken_passage/controllers/MainActivity.java
@@ -1,7 +1,9 @@
-package enal1586.ju.viken_passage;
+package enal1586.ju.viken_passage.controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import enal1586.ju.viken_passage.R;
 
 public class MainActivity extends AppCompatActivity {
 
