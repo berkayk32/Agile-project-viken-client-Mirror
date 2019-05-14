6a1a6362d1862ecf7ae5a529d0a19caa2891aa7b
Berkay Torun <berkayk32@hotmail.com>
2019-05-14 22:26:18 +0200

    Added Geopoint Normal form on history and changed possible Object reference error

--- vikenpassage/app/src/main/java/enal1586/ju/viken_passage/models/HistoryModel.java
+++ vikenpassage/app/src/main/java/enal1586/ju/viken_passage/models/HistoryModel.java
@@ -1,15 +1,21 @@
 package enal1586.ju.viken_passage.models;
 
+import android.location.Geocoder;
+
+import com.google.firebase.firestore.GeoPoint;
+
 import java.util.Date;
 
 public class HistoryModel {
 
     String _payment;
     Date _date;
+    GeoPoint _geopoint;
 
-    public HistoryModel(Date Date, String Payment) {
+    public HistoryModel(Date Date, String Payment,GeoPoint geoPoint) {
         this._payment = Payment;
         this._date = Date;
+        this._geopoint = geoPoint;
 
     }
 
@@ -20,6 +26,8 @@ public class HistoryModel {
     public Date getDate() {
         return _date;
     }
+
+    public GeoPoint getGeopoint() {return _geopoint; }
 
 }
 
